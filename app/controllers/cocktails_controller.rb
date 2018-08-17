1c3f23c8f03a0d1a56768bf79c531ec961414190
Eric Foucault <efoucault@gmail.com>
2018-08-17 10:57:13 +0200

    ok

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,34 @@
+class CocktailsController < ApplicationController
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  def show
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @cocktail = Cocktail.create!(cocktail_params)
+    redirect_to cocktails_path
+  end
+
+  def edit
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def update
+    @cocktail = Cocktail.find(params[:id])
+    @cocktail.update(cocktail_params)
+    redirect_to cocktails_path
+  end
+end
+
+private
+
+def cocktail_params
+  params.require(:cocktail).permit(:name, :picture)
+end
